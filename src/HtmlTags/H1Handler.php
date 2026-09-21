f1c4479cd0369cd8b2e360cce2455ac8c6c5a59a
BelKoD <dr.belkod@yandex.ru>
2026-09-21 15:59:02 +0300

    up

---
+++ src/HtmlTags/H1Handler.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace BelKoD\OdtGenerator\HtmlTags;
+
+use BelKoD\OdtGenerator\Interfaces\BlockInterface;
+
+/**
+ * Генератор заголовков H1-H6.
+ */
+class H1Handler extends TagHandler implements BlockInterface
+{
+
+    public function __construct() { }
+
+    /**
+     * @inheritDoc
+     */
+    public function handle(\DOMNode $node, array &$paragraphs)
+    {
+        $styleName = $this->style($node, ['forParagraph' => true]);
+        $content = $this->build($node);
+
+        $style = $styleName ? sprintf('text:style-name="%1"', $styleName) : '';
+        $paragraphs[] = sprintf('<text:h text:outline-level="%1" %2>%3</text:h>', 
+            $level, $style, $content);
+    }
+}
