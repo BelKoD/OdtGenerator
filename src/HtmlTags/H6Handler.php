0263f4b72aef95b79f04b73807f3b20a132b94f4
BelKoD <dr.belkod@yandex.ru>
2026-09-21 16:10:20 +0300

    create classes H2-H6

---
+++ src/HtmlTags/H6Handler.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace BelKoD\OdtGenerator\HtmlTags;
+
+use BelKoD\OdtGenerator\Interfaces\BlockInterface;
+
+/**
+ * Генератор заголовков H1-H6.
+ */
+class H6Handler extends TagHandler implements BlockInterface
+{
+
+    public function __construct() { }
+
+    /**
+     * @inheritDoc
+     */
+    public function handle(\DOMNode $node, array &$paragraphs)
+    {
+        $styleName = $this->style($node, ['forParagraph' => true]);
+        $content = $this->build($node);
+
+        $style = $styleName ? sprintf('text:style-name="%s"', $styleName) : '';
+        $paragraphs[] = sprintf('<text:h text:outline-level="6" %s>%s</text:h>', 
+            $style, $content);
+    }
+}
