5083e8f41166ca7e1ef68d0292227b5bc4ebc988
BelKoD <dr.belkod@yandex.ru>
2026-09-21 15:31:40 +0300

    change create class, add Html, Body class

---
+++ src/HtmlTags/HtmlHandler.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace BelKoD\OdtGenerator\HtmlTags;
+
+use BelKoD\OdtGenerator\Interfaces\BlockInterface;
+
+/**
+ * Генератор контейнера.
+ */
+class HtmlHandler extends TagHandler implements BlockInterface
+{
+    public function __construct() { }
+
+    /**
+     * @inheritDoc
+     */
+    public function handle(\DOMNode $node, array &$paragraphs)
+    {
+        // Контейнеры (html, body) — не добавляют контент, но обходят детей
+        foreach ($node->childNodes as $child) {
+            $this->factory->getGenerator()->processNode($child, $paragraphs);
+        }
+    }
+}
--- src/TagHandlerFactory.php
+++ src/TagHandlerFactory.php
@@ -75,7 +75,7 @@ class TagHandlerFactory
 
         $options['css'] = $css;
         $tagName = \strtolower($node->tagName);
-        // Преобразуем имя тега в имя класса (например, 'ol' -> 'Ol', 'h1' -> 'H1')
+        // Преобразуем имя тега в имя класса (например, 'ol' - 'Ol', 'h1' - 'H1')
         $classNamePart = \ucfirst($tagName);
 
         // Список классов-обработчиков в пространстве имен HtmlTags
@@ -93,7 +93,7 @@ class TagHandlerFactory
                 $tag =  new $handlerClass($options);
             }
         } else {
-            /*if ($tagName === 'p') {
+            if ($tagName === 'p') {
                 $tag = new PHandler();
             } elseif (\in_array($tagName, ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'])) {
                 $tag = new HeadingHandler();
@@ -130,7 +130,7 @@ class TagHandlerFactory
                 $tag = new ImgHandler();
             } else {
                 $tag = new IgnoredTagHandler();
-            }*/
+            }
             $tag = new IgnoredTagHandler();
         }
         $tag->setFactory($this);
